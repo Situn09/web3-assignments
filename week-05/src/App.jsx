d9845959d22503519e5c26caff7545397cc2150f
Situn09 <chiranjivrao37@gmail.com>
2025-04-22 19:40:32 +0530

    complete week 5 topic

--- week-05/src/App.jsx
+++ week-05/src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
+import React, { useMemo } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import { clusterApiUrl } from "@solana/web3.js";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import RequestAirDrop from "./Airdrop";
+import ShowSolBalance from "./Balance";
+import SendTokens from "./SendTokens";
+import SignMessage from "./SignMessage";
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
-    <>
-     100xdevs
-    </>
-  )
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <WalletMultiButton />
+            <WalletDisconnectButton />
+          </div>
+          <RequestAirDrop />
+          <ShowSolBalance />
+          <SendTokens />
+          <SignMessage />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
--- week-05/src/Balance.jsx
+++ week-05/src/Balance.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
-const Balance = () => {
+export default function ShowSolBalance() {
+  const { connection } = useConnection();
+  const wallet = useWallet();
+
+  async function getBalance() {
+    if (wallet.publicKey) {
+      const balance = await connection.getBalance(wallet.publicKey);
+      document.getElementById("balance").innerHTML = balance / LAMPORTS_PER_SOL;
+    }
+  }
+
+  getBalance();
   return (
     <div>
-      
+      <p>SOL Balance:</p> <div id="balance"></div>
     </div>
-  )
+  );
 }
-
-export default Balance
